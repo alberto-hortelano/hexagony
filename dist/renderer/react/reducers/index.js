2a70f0efd00be5fa08ee3ce73333665e30ef1c38
alberto hortelano <alberto.hortelano.moran@gmail.com>
2019-03-14 19:14:49 +0100

    Remove more folders

--- dist/renderer/react/reducers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const initialState_1 = require("./initialState");
-function selectCell(state = initialState_1.initialState, action) {
-    console.log("selectCell", state, action);
-    return state;
-}
-exports.selectCell = selectCell;
-//# sourceMappingURL=index.js.map
--- dist/renderer/react/store/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redux_1 = require("redux");
-const reducers_1 = require("../reducers");
-const store = redux_1.createStore(redux_1.combineReducers({ selectCell: reducers_1.selectCell }));
-exports.default = store;
-//# sourceMappingURL=index.js.map
